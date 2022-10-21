1f1ee7e421e526dd5111de6ddd2126cd4c2abbd3
Denis <dmel01@icloud.com>
2022-10-21 21:06:31 +0400

    Stage 3 completed

--- Chuck Norris Cipher Encoder/task/src/chucknorris/Main.java
+++ Chuck Norris Cipher Encoder/task/src/chucknorris/Main.java
@@ -10,9 +10,37 @@ public class Main {
         System.out.println("The result:");
         for (char currentChar : inputStr.toCharArray()
         ) {
-            String binaryRepresent = String.format("%07d", Integer.parseInt(Integer.toBinaryString(currentChar)));
-            System.out.printf("%c = %s%n", currentChar, binaryRepresent);
+              String binaryRepresent = String.format("%07d", Integer.parseInt(Integer.toBinaryString(currentChar)));
+            System.out.print(chuckNorrisRepresentation(binaryRepresent)+" ");
+
         }
+    }
+
+    public static String chuckNorrisRepresentation(String str) {
+        StringBuilder chuckRepresent = new StringBuilder();
+        boolean isFirstBlock = true;
+
+        char[] chars = str.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (isFirstBlock) {
+                if (chars[i] == '1') {
+                    chuckRepresent.append("0 0");
+                } else {
+                    chuckRepresent.append("00 0");
+                }
+                isFirstBlock = false;
+            } else {
+                if (chars[i] == chars[i - 1]) {
+                    chuckRepresent.append("0");
+                } else {
+                    chuckRepresent.append(" ");
+                    isFirstBlock = true;
+                    i--;
+                }
+            }
+
+        }
+        return chuckRepresent.toString();
     }
 
 }
