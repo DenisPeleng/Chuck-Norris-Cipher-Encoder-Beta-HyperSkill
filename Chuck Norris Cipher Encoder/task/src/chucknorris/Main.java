fe2bf10211e3b58214d081511477605d9c394b0f
Denis <dmel01@icloud.com>
2022-10-22 18:43:42 +0400

    Stage 4 completed

--- Chuck Norris Cipher Encoder/task/src/chucknorris/Main.java
+++ Chuck Norris Cipher Encoder/task/src/chucknorris/Main.java
@@ -5,18 +5,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Input string:");
+        System.out.println("Input encoded string:");
         String inputStr = scanner.nextLine();
         System.out.println("The result:");
-        for (char currentChar : inputStr.toCharArray()
-        ) {
-              String binaryRepresent = String.format("%07d", Integer.parseInt(Integer.toBinaryString(currentChar)));
-            System.out.print(chuckNorrisRepresentation(binaryRepresent)+" ");
-
-        }
+        System.out.println(chuckNorrisDecoder(inputStr));
     }
 
-    public static String chuckNorrisRepresentation(String str) {
+
+    public static String strToChuckEncode(String inputStr) {
+        StringBuilder fullStrInChuck = new StringBuilder();
+        for (char currentChar : inputStr.toCharArray()) {
+            String binaryRepresent = String.format("%07d", Integer.parseInt(Integer.toBinaryString(currentChar)));
+            fullStrInChuck.append(chuckNorrisEncoderBinary(binaryRepresent)).append(" ");
+        }
+        return fullStrInChuck.toString();
+    }
+
+    public static String chuckNorrisEncoderBinary(String str) {
         StringBuilder chuckRepresent = new StringBuilder();
         boolean isFirstBlock = true;
 
@@ -43,4 +48,24 @@ public class Main {
         return chuckRepresent.toString();
     }
 
+    public static String chuckNorrisDecoder(String str) {
+        String[] strInChuckFormat = str.split(" ");
+        StringBuilder binaryRepresent = new StringBuilder();
+        String firstSymbol;
+        for (int i = 0; i < strInChuckFormat.length; i = i + 2) {
+            if (strInChuckFormat[i].equals("00")) {
+                firstSymbol = "0";
+            } else {
+                firstSymbol = "1";
+            }
+            binaryRepresent.append(firstSymbol.repeat(strInChuckFormat[i + 1].toCharArray().length));
+        }
+        char[] binaryArr = binaryRepresent.toString().toCharArray();
+        StringBuilder stringFromBinary = new StringBuilder();
+        for (int i = 0; i < binaryArr.length; i = i + 7) {
+            char chrFromBin = (char) Integer.parseInt(binaryRepresent.substring(i, i + 7), 2);
+            stringFromBinary.append(chrFromBin);
+        }
+        return stringFromBinary.toString();
+    }
 }
